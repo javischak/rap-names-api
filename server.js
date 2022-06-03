3822a0865443e9bcc01ac9111b7c762eb2007cd5
javischak <martinthomas@Martins-MacBook-Air.local>
2022-06-02 18:23:23 -0700

    added cors

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const { response } = require('express')
 const express = require('express')
 const app = express()
 const PORT = 8000
+const cors = require('cors')
+
+app.use(cors())
 
 const rappers = {
     '21 Savage': {
